4cf300fb8bf3eddd3594684d5326a8d9d3e57ec1
Divya280930 <divya.vinzcs@gmail.com>
2024-02-02 20:10:20 +0530

    2ndfebcommit

--- src/test.ts
+++ src/test.ts
@@ -1,1 +1,8 @@
-console.log("Test Run");
+//declaring fn in ts
+
+function intro (tagname: string , tagnum: number): string {
+       return 'My name is ${tagname} and age is {tagnum}';
+    
+}
+
+console.log(intro( "yyy" ,  88));
